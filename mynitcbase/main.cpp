0340f17c4da0fb4d10e8667a54cb4ab5e7013e48
mihirpa18 <punchithayamihir@gmail.com>
2025-01-07 18:03:12 +0530

    Stage 2

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -5,31 +5,107 @@
 #include <cstring>
 #include <iostream>
 
-int main(int argc, char *argv[])
+void printRelation()
 {
   Disk disk_run;
-  // unsigned char buffer[BLOCK_SIZE];
+  //Creates buffers for relation catalog and attribute catalog 
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
-  // Disk::readBlock(buffer, 7000);
+  HeadInfo relCatHeader; // Stores block header information
+  HeadInfo attrCatHeader;
 
-  // char message[] = "hello world my name is mihir";
-  // memcpy(buffer + 20, message, 10);
-  // Disk::writeBlock(buffer, 7000);
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
 
-  // unsigned char buffer2[BLOCK_SIZE];
-  // char message2[10];
-  // Disk::readBlock(buffer2, 7000);
-  // memcpy(message2, buffer2 + 20, 10);
+  for (int i = 0; i < relCatHeader.numEntries; i++)
+  {
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // Stores relation catalog records
+    relCatBuffer.getRecord(relCatRecord, i);
 
-  // std::cout << message2 << std::endl;
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
 
-  unsigned char BAM[BLOCK_SIZE];
-  Disk::readBlock(BAM,0);
+    int currBlock = ATTRCAT_BLOCK; //    // first block of attribute catalog
 
-  for(int i=0;i<BLOCK_ALLOCATION_MAP_SIZE+100;i++){
-    std::cout<<(int)BAM[i] << " ";
-    
+    while (currBlock != -1)  //it should not exceed 19(total number of attributes)
+    {
+      RecBuffer currAttrBuffer(currBlock);
+      HeadInfo currAttrHeader;
+      currAttrBuffer.getHeader(&currAttrHeader);
+
+      for (int j = 0; j < currAttrHeader.numEntries; j++)
+      {
+        Attribute attrCatRecord[ATTRCAT_NO_ATTRS]; // Stores attribute catalog records
+        currAttrBuffer.getRecord(attrCatRecord, j);
+
+        if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0)
+        {
+          const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+          printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+        }
+      }
+
+      currBlock = currAttrHeader.rblock;
+    }
+    printf("\n");
   }
-  // return FrontendInterface::handleFrontend(argc, argv);
+}
+
+void exercise2()
+{
+  Disk disk_run;
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for (int i = 0; i < relCatHeader.numEntries; i++)
+  {
+    Attribute relCatRecord[RELCAT_NO_ATTRS];
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    // first block of attribute catalog
+    int currBlock = ATTRCAT_BLOCK;
+
+    while (currBlock != -1)
+    {
+      RecBuffer currAttrBuffer(currBlock);
+      HeadInfo currAttrHeader;
+      currAttrBuffer.getHeader(&currAttrHeader);
+
+      for (int j = 0; j < currAttrHeader.numEntries; j++)
+      {
+        Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+        currAttrBuffer.getRecord(attrCatRecord, j);
+
+        if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, "Students") == 0 && strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "Batch") == 0)
+        {
+              strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,"Class");
+
+              attrCatBuffer.setRecord(attrCatRecord,j); //to save the change
+          
+        }
+        
+      }
+
+      currAttrBuffer.getHeader(&currAttrHeader);
+      currBlock = currAttrHeader.rblock;
+    }
+    printf("\n");
+  }
+}
+
+int main(int argc, char *argv[])
+{
+
+  printRelation();
+  exercise2();
+  printRelation();
   return 0;
 }
